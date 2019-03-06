f0b408843f181902c077948af9b84219c12145b6
Ravi Mude <ravi.mude@jda.com>
2019-03-07 01:28:34 +0530

    Converting image to base64 string

--- src/main/java/hologram/HologramControllerV2.java
+++ src/main/java/hologram/HologramControllerV2.java
@@ -4,6 +4,7 @@ import hologram.enums.Template;
 import hologram.request.HologramCreationRequest;
 import io.swagger.annotations.ApiOperation;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import java.util.ArrayList;
 @RequestMapping(value = "hologram/v2/")
 public class HologramControllerV2 {
 
+  @CrossOrigin
   @RequestMapping(value = "get/allProducts", method = RequestMethod.GET)
   @ApiOperation(value = "Get all products available in the repository")
   public ResponseEntity<List<String>> getAllProducts() {
@@ -40,6 +42,7 @@ public class HologramControllerV2 {
     return new ResponseEntity<>(allProductsList, headers, HttpStatus.OK);
   }
 
+  @CrossOrigin
   @RequestMapping(value = "get/allTemplates", method = RequestMethod.GET)
   @ApiOperation(value = "Get all templates available")
   public ResponseEntity<List<String>> getAllTemplates() {
@@ -51,9 +54,10 @@ public class HologramControllerV2 {
     return new ResponseEntity<>(allTemplates, headers, HttpStatus.OK);
   }
 
-  @RequestMapping(value = "image/gif/generator", method = RequestMethod.POST)
+  @CrossOrigin
+  @RequestMapping(value = "image/gif/generator1", method = RequestMethod.POST)
   @ApiOperation(value = "Generate Hologram Gif from simple image")
-  public ResponseEntity<byte[]> hologramGifGenerator(@RequestBody HologramCreationRequest hologramCreationRequest) {
+  public ResponseEntity<byte[]> hologramGifGenerator1(@RequestBody HologramCreationRequest hologramCreationRequest) {
     File writeFile;
     byte[] b = null;
     try {
@@ -82,6 +86,38 @@ public class HologramControllerV2 {
     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
+  @CrossOrigin
+  @RequestMapping(value = "image/gif/generator", method = RequestMethod.POST)
+  @ApiOperation(value = "Generate Hologram Gif from simple image")
+  public ResponseEntity<byte[]> hologramGifGenerator(@RequestBody HologramCreationRequest hologramCreationRequest) {
+    File writeFile;
+    byte[] output = null;
+    try {
+      String productName = hologramCreationRequest.getProductName();
+      String offerText = hologramCreationRequest.getOfferText();
+      Template template = hologramCreationRequest.getTemplate();
+      File file = ResourceUtils.getFile("classpath:img/"+productName+"/"+productName+".png");
+      BufferedImage image = ImageIO.read(file);
+      writeFile = new File(file.getParent()+"_"+template.getValue().toLowerCase()+".gif");
+      ImageOutputStream imageOutputStream = new FileImageOutputStream(writeFile);
+      if(Template.ROUND_ROTATION.equals(template)) {
+        GifCreator.createRoundRotationGif(image, imageOutputStream, offerText);
+      } else if(Template.ZOOM_EFFECT.equals(template)) {
+        GifCreator.createZoomGif(image, imageOutputStream, offerText);
+      }
+      output = Base64.encodeBase64(Files.readAllBytes(writeFile.toPath()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    if (output != null) {
+      final HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.IMAGE_GIF);
+      return new ResponseEntity<>(output, headers, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @CrossOrigin
   @RequestMapping(value = "image/video/generator", method = RequestMethod.POST)
   @ApiOperation(value = "Generate Hologram Gif from simple image")
   public ResponseEntity hologramVideoGenerator(@RequestBody HologramCreationRequest hologramCreationRequest) {
